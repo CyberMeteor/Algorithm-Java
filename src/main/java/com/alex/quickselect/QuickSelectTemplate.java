7a67f6d9a2eb9dbec318b3537d7e60db1c9bd2ae
Ruiyang Chen <im.ruiyangchen@gmail.com>
2025-03-06 00:19:09 -0500

    Implement quick select template

---
+++ src/main/java/com/alex/quickselect/QuickSelectTemplate.java
@@ -0,0 +1,41 @@
+package com.alex.quickselect;
+
+public class QuickSelectTemplate {
+    // find kth largest element in an array
+    public int findKthLargest(int[] nums, int k) {
+        int low = 0, high = nums.length - 1;
+        int target = nums.length - k;
+
+        while (low <= high) {
+            int pivot = partition(nums, low, high);
+            if (pivot == target) {
+                return nums[pivot];
+            }  else if (pivot < target) {
+                low = pivot + 1;
+            }  else {
+                high = pivot - 1;
+            }
+        }
+        return -1;
+    }
+
+    private int partition(int[] nums, int low, int high) {
+        int pivot = nums[low];
+        int left = low + 1, right = high;
+
+        while (left <= right) {
+            if (nums[left] > pivot && nums[right] < pivot) swap(nums, left, right);
+
+            if (nums[left] <= pivot) left++;
+            if (nums[right] >= pivot) right--;
+        }
+        swap(nums, low, right);
+        return right;
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
