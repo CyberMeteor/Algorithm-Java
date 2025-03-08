14caa6396094bcbeb5308e093d64219fcb755414
Ruiyang Chen <im.ruiyangchen@gmail.com>
2025-03-08 00:13:23 -0500

    Implement heap sort template

--- src/main/java/com/alex/sort/SortTemplate.java
+++ src/main/java/com/alex/sort/SortTemplate.java
@@ -54,6 +54,34 @@ public class SortTemplate {
         }
     }
 
+    // Heap Sort
+    public void heapSort(int[] nums) {
+        int n = nums.length;
+
+        for (int i = n / 2 - 1; i >= 0; i--) {
+            heapify(nums, n, i);
+        }
+
+        for (int i = n - 1; i > 0; i--) {
+            swap(nums, 0, i);
+            heapify(nums, i, 0);
+        }
+    }
+
+    private void heapify(int[] nums, int n, int i) {
+        int largest = i;
+        int left = 2 * i + 1;
+        int right = 2 * i + 2;
+
+        if (left < n && nums[left] > nums[largest]) largest = left;
+        if (right < n && nums[right] > nums[largest]) largest = right;
+
+        if (largest != i) {
+            swap(nums, i, largest);
+            heapify(nums, n, largest);
+        }
+    }
+
 
     /*-------------------------------- common -------------------------------------------*/
 
