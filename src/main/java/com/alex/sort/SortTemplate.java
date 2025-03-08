0a81954529d6a8831903464ddadeb5d13ff6de2a
Ruiyang Chen <im.ruiyangchen@gmail.com>
2025-03-07 23:56:22 -0500

    Implement merge sort template

--- src/main/java/com/alex/sort/SortTemplate.java
+++ src/main/java/com/alex/sort/SortTemplate.java
@@ -24,6 +24,36 @@ public class SortTemplate {
         return j;
     }
 
+    // Merge Sort
+    public void mergeSort(int[] nums, int start, int end) {
+        if (start >= end) return;
+
+        int mid = start + (end - start) / 2;
+        mergeSort(nums, start, mid);
+        mergeSort(nums, mid + 1, end);
+        merge(nums, start, mid, end);
+    }
+
+    private void merge(int[] nums, int start, int mid, int end) {
+        int[] temp = new int[end - start + 1];
+        int i = start, j = mid + 1, k = 0;
+
+        while (i <= mid && j <= end) {
+            if (nums[i] < nums[j]) {
+                temp[k++] = nums[i++];
+            } else  {
+                temp[k++] = nums[j++];
+            }
+        }
+
+        while (i <= mid) temp[k++] = nums[i++];
+        while (j <= end) temp[k++] = nums[j++];
+
+        for (int p = 0; p < temp.length; p++) {
+            nums[start + p] = temp[p];
+        }
+    }
+
 
     /*-------------------------------- common -------------------------------------------*/
 
