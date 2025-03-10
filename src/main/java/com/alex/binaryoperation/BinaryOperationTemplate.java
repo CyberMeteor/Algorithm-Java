fdf49e5d48219ff22805d8240737ff17e194ef55
Ruiyang Chen <im.ruiyangchen@gmail.com>
2025-03-10 13:56:59 -0400

    Implement binary operation template

---
+++ src/main/java/com/alex/binaryoperation/BinaryOperationTemplate.java
@@ -0,0 +1,25 @@
+package com.alex.binaryoperation;
+
+public class BinaryOperationTemplate {
+    public void operation(){
+
+        // Basic operation
+        /* ^ is xor
+        *  a = 0 ^ a = a ^ 0
+        *  0 = a ^ a
+        * */
+
+        // Swap 2 numbers
+        /*
+         *  a = a ^ b
+         *  b = a ^ b
+         *  a = a ^ b
+         * */
+
+        // Remove the last 1
+        // a = n & (n - 1)
+
+        // Obtain the last 1
+        // diff = (n & (n - 1)) ^ n
+    }
+}
