42e93e7f53fcf4c337b4946d93bd1ac6bcef6f82
Ruiyang Chen <im.ruiyangchen@gmail.com>
2025-03-16 22:55:24 -0400

    Implement valid bracket template

--- src/main/java/com/alex/stack/StackTemplate.java
+++ src/main/java/com/alex/stack/StackTemplate.java
@@ -30,4 +30,27 @@ public class StackTemplate {
         }
         return stack.pop();
     }
+
+    /**
+     * Valid bracket
+     */
+
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (char c : s.toCharArray()) {
+            switch (c) {
+                case '(':
+                    stack.push(')');
+                    break;
+                case  '[':
+                    stack.push(']');
+                    break;
+                case  '{':
+                    stack.push('}');
+                default:
+                    if (stack.isEmpty() || stack.pop() != c) return false;
+            }
+        }
+        return stack.isEmpty();
+    }
 }
