145285afe99a95b6e41f74f9e4f89304cf8859d5
Ruiyang Chen <im.ruiyangchen@gmail.com>
2025-03-21 18:23:13 -0400

    Solve Least Recently Used cache

---
+++ src/main/java/com/alex/leetcode/editor/en/[146]LRU Cache.java
@@ -0,0 +1,142 @@
+//Design a data structure that follows the constraints of a Least Recently Used 
+//(LRU) cache. 
+//
+// Implement the LRUCache class: 
+//
+// 
+// LRUCache(int capacity) Initialize the LRU cache with positive size capacity. 
+//
+// int get(int key) Return the value of the key if the key exists, otherwise 
+//return -1. 
+// void put(int key, int value) Update the value of the key if the key exists. 
+//Otherwise, add the key-value pair to the cache. If the number of keys exceeds 
+//the capacity from this operation, evict the least recently used key. 
+// 
+//
+// The functions get and put must each run in O(1) average time complexity. 
+//
+// 
+// Example 1: 
+//
+// 
+//Input
+//["LRUCache", "put", "put", "get", "put", "get", "put", "get", "get", "get"]
+//[[2], [1, 1], [2, 2], [1], [3, 3], [2], [4, 4], [1], [3], [4]]
+//Output
+//[null, null, null, 1, null, -1, null, -1, 3, 4]
+//
+//Explanation
+//LRUCache lRUCache = new LRUCache(2);
+//lRUCache.put(1, 1); // cache is {1=1}
+//lRUCache.put(2, 2); // cache is {1=1, 2=2}
+//lRUCache.get(1);    // return 1
+//lRUCache.put(3, 3); // LRU key was 2, evicts key 2, cache is {1=1, 3=3}
+//lRUCache.get(2);    // returns -1 (not found)
+//lRUCache.put(4, 4); // LRU key was 1, evicts key 1, cache is {4=4, 3=3}
+//lRUCache.get(1);    // return -1 (not found)
+//lRUCache.get(3);    // return 3
+//lRUCache.get(4);    // return 4
+// 
+//
+// 
+// Constraints: 
+//
+// 
+// 1 <= capacity <= 3000 
+// 0 <= key <= 10⁴ 
+// 0 <= value <= 10⁵ 
+// At most 2 * 10⁵ calls will be made to get and put. 
+// 
+//
+// Related Topics Hash Table Linked List Design Doubly-Linked List 👍 21588 👎 1
+//121
+
+package com.alex.leetcode.editor.en;
+
+import java.util.HashMap;
+import java.util.Map;
+
+class lruCache{
+    //2025-03-21 17:57:42
+    //LRU Cache
+    //QuestionID：[146]
+    
+    public static void main(String[] args) {
+//        Solution solution = new lruCache().new Solution();
+        // TO TEST
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+class LRUCache {
+    private static class Node {
+        int key, value;
+        Node prev, next;
+
+        Node(int k, int v) {
+            key = k;
+            value = v;
+        }
+    }
+
+    private final int capacity;
+    private final Node dummy = new Node(0, 0);
+    private final Map<Integer, Node> keyToNode = new HashMap<>();
+
+    public LRUCache(int capacity) {
+        this.capacity = capacity;
+        dummy.prev = dummy;
+        dummy.next = dummy;
+    }
+
+    public int get(int key) {
+        Node node = getNode(key);
+        return node != null ? node.value : -1;
+    }
+
+    public void put(int key, int value) {
+        Node node = getNode(key);
+        if (node != null) {
+            node.value = value;
+            return;
+        }
+        node =  new Node(key, value);
+        keyToNode.put(key, node);
+        pushFront(node);
+        if (keyToNode.size() > capacity) {
+            Node backNode = dummy.prev;
+            keyToNode.remove(backNode.key);
+            remove(backNode);
+        }
+    }
+
+    private Node getNode(int key) {
+        if (!keyToNode.containsKey(key)) {
+            return null;
+        }
+        Node node = keyToNode.get(key);
+        remove(node);
+        pushFront(node);
+        return node;
+    }
+
+    private void remove(Node x) {
+        x.prev.next = x.next;
+        x.next.prev = x.prev;
+    }
+
+    private void pushFront(Node x) {
+        x.prev = dummy;
+        x.next = dummy.next;
+        x.prev.next = x;
+        x.next.prev = x;
+    }
+}
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache obj = new LRUCache(capacity);
+ * int param_1 = obj.get(key);
+ * obj.put(key,value);
+ */
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
