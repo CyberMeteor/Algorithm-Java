8b1b2ada4889a5eeca8f2a773bbc7b478e4073d1
Ruiyang Chen <im.ruiyangchen@gmail.com>
2025-03-22 21:20:39 -0400

    Solve Reverse Linked List

---
+++ src/main/java/com/alex/leetcode/editor/en/[206]Reverse Linked List.java
@@ -0,0 +1,75 @@
+//Given the head of a singly linked list, reverse the list, and return the 
+//reversed list. 
+//
+// 
+// Example 1: 
+// 
+// 
+//Input: head = [1,2,3,4,5]
+//Output: [5,4,3,2,1]
+// 
+//
+// Example 2: 
+// 
+// 
+//Input: head = [1,2]
+//Output: [2,1]
+// 
+//
+// Example 3: 
+//
+// 
+//Input: head = []
+//Output: []
+// 
+//
+// 
+// Constraints: 
+//
+// 
+// The number of nodes in the list is the range [0, 5000]. 
+// -5000 <= Node.val <= 5000 
+// 
+//
+// 
+// Follow up: A linked list can be reversed either iteratively or recursively. 
+//Could you implement both? 
+//
+// Related Topics Linked List Recursion 👍 22669 👎 513
+
+package com.alex.leetcode.editor.en;
+class reverseLinkedList{
+    //2025-03-22 21:13:40
+    //Reverse Linked List
+    //QuestionID：[206]
+    
+    public static void main(String[] args) {
+        Solution solution = new reverseLinkedList().new Solution();
+        // TO TEST
+    }
+    //Definition for singly-linked list.
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode pre =  null;
+        ListNode cur = head;
+        while (cur != null) {
+            ListNode next = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = next;
+        }
+        return pre;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
