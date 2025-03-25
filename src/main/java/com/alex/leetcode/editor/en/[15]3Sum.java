08aa02a24dc12b652ce403b3d61a40b92e180c4c
Ruiyang Chen <im.ruiyangchen@gmail.com>
2025-03-24 22:31:43 -0400

    Solve 3Sum and reverse node in k group

---
+++ src/main/java/com/alex/leetcode/editor/en/[15]3Sum.java
@@ -0,0 +1,93 @@
+//Given an integer array nums, return all the triplets [nums[i], nums[j], nums[
+//k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0. 
+//
+// Notice that the solution set must not contain duplicate triplets. 
+//
+// 
+// Example 1: 
+//
+// 
+//Input: nums = [-1,0,1,2,-1,-4]
+//Output: [[-1,-1,2],[-1,0,1]]
+//Explanation: 
+//nums[0] + nums[1] + nums[2] = (-1) + 0 + 1 = 0.
+//nums[1] + nums[2] + nums[4] = 0 + 1 + (-1) = 0.
+//nums[0] + nums[3] + nums[4] = (-1) + 2 + (-1) = 0.
+//The distinct triplets are [-1,0,1] and [-1,-1,2].
+//Notice that the order of the output and the order of the triplets does not 
+//matter.
+// 
+//
+// Example 2: 
+//
+// 
+//Input: nums = [0,1,1]
+//Output: []
+//Explanation: The only possible triplet does not sum up to 0.
+// 
+//
+// Example 3: 
+//
+// 
+//Input: nums = [0,0,0]
+//Output: [[0,0,0]]
+//Explanation: The only possible triplet sums up to 0.
+// 
+//
+// 
+// Constraints: 
+//
+// 
+// 3 <= nums.length <= 3000 
+// -10⁵ <= nums[i] <= 10⁵ 
+// 
+//
+// Related Topics Array Two Pointers Sorting 👍 32506 👎 3062
+
+package com.alex.leetcode.editor.en;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+class threeSum{
+    //2025-03-24 22:29:32
+    //3Sum
+    //QuestionID：[15]
+    
+    public static void main(String[] args) {
+        Solution solution = new threeSum().new Solution();
+        // TO TEST
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> ans = new ArrayList<>();
+        int n = nums.length;
+        for (int i = 0; i < n - 2; i++) {
+            int x = nums[i];
+            if (i > 0 && x == nums[i - 1]) continue;
+            if (x + nums[i + 1] + nums[i + 2] > 0) break; // 优化一
+            if (x + nums[n - 2] + nums[n - 1] < 0) continue; // 优化二
+            int j = i + 1;
+            int k = n - 1;
+            while (j < k) {
+                int s = x + nums[j] + nums[k];
+                if (s > 0) {
+                    k--;
+                } else if (s < 0) {
+                    j++;
+                } else { // 三数之和为 0
+                    ans.add(List.of(x, nums[j], nums[k]));
+                    for (j++; j < k && nums[j] == nums[j - 1]; j++);
+                    for (k--; k > j && nums[k] == nums[k + 1]; k--);
+                }
+            }
+        }
+        return ans;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
---
+++ src/main/java/com/alex/leetcode/editor/en/[25]Reverse Nodes in k-Group.java
@@ -0,0 +1,100 @@
+//Given the head of a linked list, reverse the nodes of the list k at a time, 
+//and return the modified list. 
+//
+// k is a positive integer and is less than or equal to the length of the 
+//linked list. If the number of nodes is not a multiple of k then left-out nodes, in 
+//the end, should remain as it is. 
+//
+// You may not alter the values in the list's nodes, only nodes themselves may 
+//be changed. 
+//
+// 
+// Example 1: 
+// 
+// 
+//Input: head = [1,2,3,4,5], k = 2
+//Output: [2,1,4,3,5]
+// 
+//
+// Example 2: 
+// 
+// 
+//Input: head = [1,2,3,4,5], k = 3
+//Output: [3,2,1,4,5]
+// 
+//
+// 
+// Constraints: 
+//
+// 
+// The number of nodes in the list is n. 
+// 1 <= k <= n <= 5000 
+// 0 <= Node.val <= 1000 
+// 
+//
+// 
+// Follow-up: Can you solve the problem in O(1) extra memory space? 
+//
+// Related Topics Linked List Recursion 👍 14432 👎 744
+
+package com.alex.leetcode.editor.en;
+class reverseNodesInKGroup{
+    //2025-03-23 20:06:08
+    //Reverse Nodes in k-Group
+    //QuestionID：[25]
+    
+    public static void main(String[] args) {
+        Solution solution = new reverseNodesInKGroup().new Solution();
+        // TO TEST
+    }
+
+    //Definition for singly-linked list.
+    private class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseKGroup(ListNode head, int k) {
+        int n = 0;
+        for (ListNode cur = head; cur != null; cur = cur.next) {
+            n++;
+        }
+        ListNode dummy = new ListNode(0, head);
+        ListNode p0 = dummy;
+        ListNode pre = null;
+        ListNode cur = head;
+
+        for (; n >= k; n -= k) {
+            for (int i = 0; i < k; i++) {
+                ListNode nxt = cur.next;
+                cur.next = pre;
+                pre = cur;
+                cur = nxt;
+            }
+
+            ListNode nxt = p0.next;
+            p0.next.next = cur;
+            p0.next = pre;
+            p0 = nxt;
+        }
+        return dummy.next;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
